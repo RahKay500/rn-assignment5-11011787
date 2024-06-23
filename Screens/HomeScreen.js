e5d8debc502c80d4c3ad5c27931a58badf8065aa
RahKay500 <rakayor@st.ug.edu.gh>
2024-06-23 23:26:18 +0000

    Added a ThemeContext file and modified the other Screens

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,91 +1,102 @@
     import React from 'react';
     import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+    import { useTheme } from '../ThemeContext';
 
     const transactions = [
     { id: '1',  name: 'Apple Store', description: ' Entertainment', amount: '- $5.99', icon: require('../assets/apple.png') },
     { id: '2',  name: 'Spotify', description: ' Music', amount: '- $12.99', icon: require('../assets/spotify.png') },
-    { id: '3',  name: 'Money Transfer', description: ' Transaction', amount: '$3.00', icon: require('../assets/moneyTransfer.png') },
+    { id: '3',  name: 'Money Transfer', description: ' Transaction', amount: '$300', icon: require('../assets/moneyTransfer.png') },
     { id: '4',  name: 'Grocery ', description: ' Shopping', amount: '- $88', icon: require('../assets/grocery.png') },
     ];
 
     export default function HomeScreen() {
+    const { isDarkTheme } = useTheme();
+    const styles = createStyles(isDarkTheme);
+
     return (
         <View style={styles.container}>
-            <View style= {styles.headerContainer}>
-                <Image style={styles.profileImage} source={require('../assets/RahKayy.jpg')} />
-                <Text style={styles.welcomeText}>Welcome back,</Text>
-                <Text style={styles.name}>Rahina Kayor</Text>
-                <View style={styles.searchContainer}>
-                    <Image style={styles.searchIcon} source={require('../assets/search.png')} />
-                </View>
+        <View style={styles.headerContainer}>
+            <Image style={styles.profileImage} source={require('../assets/RahKayy.jpg')} />
+            <Text style={styles.welcomeText}>Welcome back,</Text>
+            <Text style={styles.name}>Rahina Kayor</Text>
+            <View style={styles.searchContainer}>
+            <Image style={[styles.icons, {tintColor: isDarkTheme ? 'white' : 'black'}]} source={require('../assets/search.png')} />
             </View>
-            <View style={styles.paymentInfo}>
-                <Image style={styles.creditCard} source={require('../assets/Card.png')} />
-                <View style={styles.paymentOptionsContainers}>
-                    <View style={styles.paymentOptions}>
-                        <View style={styles.iconsContainer}>
-                            <Image style={styles.icons} source={require('../assets/send.png')} />
-                        </View>
-                        <Text style={styles.optionsText}>Sent</Text>
-                    </View>
-                    <View style={styles.paymentOptions}>
-                        <View style={styles.iconsContainer}>
-                            <Image style={styles.icons} source={require('../assets/recieve.png')} />
-                        </View>
-                        <Text style={styles.optionsText}>Receive</Text>
-                    </View>
-                    <View style={styles.paymentOptions}>
-                        <View style={styles.iconsContainer}>
-                            <Image style={styles.icons} source={require('../assets/loan.png')} />
-                        </View>
-                        <Text style={styles.optionsText}>Loan</Text>
-                    </View>
-                    <View style={styles.paymentOptions}>
-                        <View style={styles.iconsContainer}>
-                            <Image style={styles.icons} source={require('../assets/topUp.png')} />
-                        </View>
-                        <Text style={styles.optionsText}>Topup</Text>
-                    </View>
+        </View>
+        <View style={styles.paymentInfo}>
+            <Image style={styles.creditCard} source={require('../assets/Card.png')} />
+            <View style={styles.paymentOptionsContainers}>
+            <View style={styles.paymentOptions}>
+                <View style={styles.iconsContainer}>
+                <Image style={[styles.icons, {tintColor: isDarkTheme ? 'white' : 'black'}]} 
+                source={require('../assets/send.png')} />
                 </View>
+                <Text style={styles.optionsText}>Sent</Text>
             </View>
-            <View style={styles.transactionsContainer}>
-                <Text style={styles.transactionText}>Transaction</Text>
-                <Text style= {{color: 'blue'}}>See All</Text>
-            </View>
-            <FlatList
-                data={transactions}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                <View style={styles.transactions}>
-                    <View style={styles.iconsContainer}>
-                        <Image source={item.icon} />
-                    </View>
-                    <View style={styles.transactionDetails}>
-                        <Text style={styles.transactionName}>{item.name}</Text>
-                        <Text style={styles.transactionDescription}>{item.description}</Text>
-                    </View>
-                    <Text style={[styles.transactionAmount, 
-                        { color: item.amount.includes('$3.00') ? 'blue' : 'black' }]}>
-                        {item.amount}
-                    </Text>
+            <View style={styles.paymentOptions}>
+                <View style={styles.iconsContainer}>
+                <Image style={[styles.icons, {tintColor: isDarkTheme ? 'white' : 'black'}]} 
+                source={require('../assets/recieve.png')} />
                 </View>
-                )}
-            />
-        </View> 
+                <Text style={styles.optionsText}>Receive</Text>
+            </View>
+            <View style={styles.paymentOptions}>
+                <View style={styles.iconsContainer}>
+                <Image style={[styles.icons, {tintColor: isDarkTheme ? 'white' : 'black'}]} 
+                source={require('../assets/loan.png')} />
+                </View>
+                <Text style={styles.optionsText}>Loan</Text>
+            </View>
+            <View style={styles.paymentOptions}>
+                <View style={styles.iconsContainer}>
+                <Image style={[styles.icons, {tintColor: isDarkTheme ? 'white' : 'black'}]} 
+                source={require('../assets/topUp.png')} />
+                </View>
+                <Text style={styles.optionsText}>Topup</Text>
+            </View>
+            </View>
+        </View>
+        <View style={styles.transactionsContainer}>
+            <Text style={styles.transactionText}>Transaction</Text>
+            <Text style={{ color: 'blue' }}>Sell All</Text>
+        </View>
+        <FlatList
+            data={transactions}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+            <View style={styles.transactions}>
+                <View style={styles.iconsContainer}>
+                <Image style={item.name === 'Spotify'
+                ? null : { tintColor: isDarkTheme ? 'white' : 'black' }}
+                source={item.icon} />
+                </View>
+                <View style={styles.transactionDetails}>
+                <Text style={styles.transactionName}>{item.name}</Text>
+                <Text style={styles.transactionDescription}>{item.description}</Text>
+                </View>
+                <Text style={[ 
+                styles.transactionAmount, 
+                { color: item.amount.includes('$300') ? 'blue' : (isDarkTheme ? 'white' : 'black') }]}>
+                {item.amount}
+                </Text>
+            </View>
+            )}
+        />
+        </View>
     );
     }
 
-    const styles = StyleSheet.create({
+    const createStyles = (isDarkTheme) => StyleSheet.create({
     container: { 
         flex: 1, 
         flexDirection: 'column',
         padding: 50,
-        backgroundColor: '#ffffff', 
+        backgroundColor: isDarkTheme ? '#0A0115' : '#ffffff', 
         paddingTop: 50,
     },
     headerContainer: { 
         flexDirection: 'row', 
-        justifyContent:'space-between', 
+        justifyContent: 'space-between', 
         alignItems: 'center', 
         marginBottom: 20,
         top: 5,
@@ -94,7 +105,7 @@
         width: 45,
         height: 45,
         top: 30,
-        borderRadius: '100',
+        borderRadius: 100,
         backgroundColor: '#FFFFFF',
     },
     welcomeText: { 
@@ -105,6 +116,7 @@
         fontFamily: 'Poppins',
         opacity: 0.5,
         left: 5,
+        color: isDarkTheme ? 'white' : 'black',
     },
     name: { 
         height: 30,
@@ -115,12 +127,13 @@
         fontSize: 18, 
         fontFamily: 'Poppins',
         fontWeight: 'bold',
+        color: isDarkTheme ? 'white' : 'black',
     },
     searchContainer: {
         width: 40,
         height: 40,
-        backgroundColor: '#F8F8F8',
-        borderRadius: "100%",
+        backgroundColor: isDarkTheme ? '#1F1E2E' : '#F8F8F8',
+        borderRadius: 100,
         top: 25,
         alignItems: 'center',
         justifyContent: 'center',
@@ -145,13 +158,20 @@
         paddingLeft: 55,
         paddingRight: 55,
     },
+    optionsText: {
+        fontSize: 14, 
+        marginTop: 5,
+        color: isDarkTheme ? 'gray' : 'black',
+    },
     iconsContainer: {
         width: 50, 
         height: 50, 
-        backgroundColor: '#F8F8F8', 
-        borderRadius: '100%', 
+        backgroundColor: isDarkTheme ? '#1F1E2E': '#F8F8F8', 
+        borderRadius: 100, 
         alignItems: 'center', 
         justifyContent: 'center',
+    },
+    icons: { 
     },
     transactionsContainer: { 
         flexDirection: 'row', 
@@ -162,6 +182,7 @@
     transactionText: { 
         fontSize: 18, 
         fontWeight: '600',
+        color: isDarkTheme ? 'white' : 'black',
     },
     transactions: { 
         flexDirection: 'row',  
@@ -175,7 +196,7 @@
         flex: 1,
     },
     transactionDescription: { 
-        color: 'gray', 
+        color: isDarkTheme ? 'lightgray' : 'gray', 
         fontSize: 12, 
         fontWeight: '400',
         position: 'relative',
@@ -188,5 +209,8 @@
         position: 'absolute',
         left: 12,
         bottom: 5,
+        color: isDarkTheme ? 'white' : 'black',
     },
+    transactionAmount: {
+    }
     });
